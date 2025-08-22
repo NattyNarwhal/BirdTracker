ce54db906e11271427e6e6340b5178c19a5baf18
Calvin Buckley <calvin@cmpct.info>
2025-08-22 20:47:03 -0300

    Initial Commit

---
+++ BirdTracker/BirdTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdTrackerApp.swift
+//  BirdTracker
+//
+//  Created by Calvin Buckley on 2025-08-22.
+//
+
+import SwiftUI
+
+@main
+struct BirdTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
