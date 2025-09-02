37261cae82db035b5bc0d1526db3660f9a789718
Calvin Buckley <calvin@cmpct.info>
2025-09-02 00:40:51 -0300

    Use a source node instead of player, which doesn't have impedence mismatch between push and pull

--- BirdTracker/ModulePlayer.swift
+++ BirdTracker/ModulePlayer.swift
@@ -16,56 +16,44 @@ class ModulePlayer: ObservableObject {
     var timer: Timer?
     
     var engine = AVAudioEngine()
-    var playerNode = AVAudioPlayerNode()
+    var sourceNode: AVAudioSourceNode!
     let format = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 2)
     
     init() {
-        engine.attach(playerNode)
-        engine.connect(playerNode, to: engine.mainMixerNode, format: format)
+        self.sourceNode = AVAudioSourceNode(format: format!, renderBlock: { silence, timestamp, frameCount, buffers in
+            guard let module = self.currentModule else {
+                print("Ope, no module")
+                return noErr
+            }
+            
+            let abl = UnsafeMutableAudioBufferListPointer(buffers)
+            let left = abl[0].mData!.assumingMemoryBound(to: Float.self)
+            let right = abl[1].mData!.assumingMemoryBound(to: Float.self)
+            
+            let count = module.readStereo(left: left, right: right, count: Int(frameCount))
+            if count == 0 {
+                silence.pointee = true
+            }
+            return noErr
+        })
+        
+        engine.attach(sourceNode)
+        engine.connect(sourceNode, to: engine.mainMixerNode, format: format)
         engine.prepare()
     }
     
     func play() {
-        guard let module = self.currentModule else {
-            print("Ope, no module")
-            return
-        }
-        let frameCount = 12800
-        guard let buffer = AVAudioPCMBuffer(pcmFormat: self.format!, frameCapacity: AVAudioFrameCount(frameCount)) else {
-            print("Ope, no buffer")
-            return
-        }
         do {
             try engine.start()
         } catch {
             print("Ope, can't start audio?")
         }
-        let interval = 1 / (self.format!.sampleRate / Double(frameCount))
-        self.timer = Timer(timeInterval: interval, repeats: true) {
-            [weak self] _ in
-            guard self?.playing  == true else {
-                return
-            }
-            guard let floatChannelData = buffer.floatChannelData else {
-                return
-            }
-            let count = module.readStereo(left: floatChannelData[0], right: floatChannelData[1], count: Int(buffer.frameCapacity))
-            if count == 0 {
-                return
-            }
-            buffer.frameLength = AVAudioFrameCount(count)
-            self?.playerNode.scheduleBuffer(buffer, at: nil, options: [.interrupts])
-        }
-        RunLoop.current.add(self.timer!, forMode: .common)
-        
-        playerNode.play()
         playing = true
     }
     
     func stop() {
         self.timer?.invalidate()
         playing = false
-        playerNode.stop()
         engine.stop()
     }
 }
