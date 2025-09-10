70b6eaaaa481e59a5182431e3a52bf81d0498509
Calvin Buckley <calvin@cmpct.info>
2025-09-10 00:37:18 -0300

    Make fixed size columns and text elements
    Another attempt to help SwiftUI
    
    also include an alternative approach commented out... this sucks

--- BirdTracker/PatternViewer.swift
+++ BirdTracker/PatternViewer.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AppKit
 
 struct PatternViewer: View {
     @Environment(\.player) private var player
@@ -29,13 +30,27 @@ struct PatternViewer: View {
             Table(of: Module.PatternRow.self, selection: Binding(get: { return highlightedRow }, set: { newValue in updatePosition(row: newValue) })) {
                 TableColumn("Row") { (row: Module.PatternRow) in
                     Text(String(row.id, radix: 16, uppercase: true))
+                        .frame(width: 30, height: 16, alignment: .trailing)
+                        .fixedSize()
                 }
                 .width(30)
+                // This is a single column for all channels, like a lot of early GUI tracker players did.
+                // This has a lot of limitations, but the performance is much better for huge patterns
+                // (i.e. 64 channels * 256 rows)
+//                TableColumn("Channels") { (row: Module.PatternRow) in
+//                    let channelCells = row.cells.map { $0.formatted }.joined(separator: "   ")
+//                    let attributedString = AttributedString(channelCells)
+//                        .settingAttributes(AttributeContainer([.ligature: 0]))
+//                    Text(attributedString)
+//                }
                 TableColumnForEach(pattern.module.channels) { channel in
                     TableColumn(channel.name) { (row: Module.PatternRow) in
                         // For now, this just uses the OpenMPT formatted text, but we could provide a richer thing here
                         Text(row.cells[Int(channel.id)].formatted)
+                            .frame(width: 120, height: 16)
+                            .fixedSize()
                     }
+                    .width(120)
                 }
             } rows: {
                 ForEach(pattern.rows) { (row: Module.PatternRow) in
